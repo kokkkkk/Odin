7be924c9f097d4779dfe40a12bf1d822a6c7dffb
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2023-03-13 21:36:04 -0400

    Change color of grid when hover

--- Etch-A-Sketch/index.js
+++ Etch-A-Sketch/index.js
@@ -1,17 +1,23 @@
 const container = document.querySelector('#container');
-container.setAttribute
-('style',
-    'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); padding: 10px;'
-)
 
 const TotalNumOfGrid = 16*16;
 
 for(i = 0; i<TotalNumOfGrid; i++){
     let div = document.createElement('div');
-    div.setAttribute
-    ('style', 
-        'border-style: solid; padding: 20px;'
-    ); 
-    
+    div.classList.add('grid');
     container.appendChild(div);
 }
+
+function changeColor(grid){
+    grid.classList.add('changeToBlue');
+    console.log("change");
+}
+
+function addListener(){
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid=>grid.addEventListener('mouseover', function(){
+        changeColor(grid);
+    }));
+}
+
+addListener();
