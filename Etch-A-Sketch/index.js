005f7d5502ccc92fbb3765cc4715c907024629ee
kokkkkk <tszshanko4-c@my.cityu.edu.hk>
2023-03-13 22:15:28 -0400

    Add button to change number of grid

--- Etch-A-Sketch/index.js
+++ Etch-A-Sketch/index.js
@@ -1,16 +1,30 @@
 const container = document.querySelector('#container');
 
-const TotalNumOfGrid = 16*16;
-
-for(i = 0; i<TotalNumOfGrid; i++){
-    let div = document.createElement('div');
-    div.classList.add('grid');
-    container.appendChild(div);
+function createGrid(num){
+    for(i = 0; i<num; i++){
+        let div = document.createElement('div');
+        div.classList.add('grid');
+        container.appendChild(div);
+    }
 }
 
 function changeColor(grid){
     grid.classList.add('changeToBlue');
-    console.log("change");
+}
+
+function changeGridNumber(){
+    let num = prompt("Please enter your the number for grids");
+
+    if (num != null) {
+        container.innerHTML = "";
+        container.setAttribute('style',`grid-template-columns: repeat(${num}, auto); grid-template-rows: repeat(${num}, auto);`)
+        if(num>=0 && num <= 100){
+            createGrid(num*num);
+        }else if(num > 100){
+            createGrid(100);
+        }
+        addListener();
+    }
 }
 
 function addListener(){
@@ -18,6 +32,13 @@ function addListener(){
     grids.forEach(grid=>grid.addEventListener('mouseover', function(){
         changeColor(grid);
     }));
+
+    const button = document.querySelector('button');
+    button.addEventListener('click',function(){
+        changeGridNumber();
+    });
 }
 
+let TotalNumOfGrid = 16*16;
+createGrid(TotalNumOfGrid);
 addListener();
