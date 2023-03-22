660d6b0c097cc6ee1db103f7e4f5772318d2e975
kokkkkk <ko.kiki12355@gmail.com>
2023-03-22 18:59:39 -0400

    Create operate function

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,3 +1,5 @@
+let number1, number2, operator;
+
 function add(num1,num2){
     return num1 + num2;
 }
@@ -12,4 +14,19 @@ function multiply(num1,num2){
 
 function divide(num1,num2){
     return num1 / num2;
+}
+
+function operate(){
+    switch(operator){
+        case '+': 
+            add(number1,number2);
+            break;
+        case '-':
+            subtract(number1,number2);
+            break;
+        case '*':
+            multiply(number1,number2);
+        case '/':
+            divide(number1,number2);
+    }
 }
