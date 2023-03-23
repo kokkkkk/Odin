1ca1dbc8ccb9c07319893b475b6bdbc9b1a90b70
kokkkkk <ko.kiki12355@gmail.com>
2023-03-22 21:14:48 -0400

    Finish calculator function

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,50 +1,86 @@
 let number1, number2, operator;
 let displayValue = "";
-const display = document.querySelector('#displayValue');
+const display = document.querySelector("#displayValue");
+let isOperatorClicked = false;
 
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+  return num1 / Number(num2);
 }
 
 function operate() {
-    switch (operator) {
-        case "+":
-            add(number1, number2);
-            break;
-        case "-":
-            subtract(number1, number2);
-            break;
-        case "*":
-            multiply(number1, number2);
-        case "/":
-            divide(number1, number2);
-    }
+  switch (operator) {
+    case "+":
+      return add(number1, number2);
+    case "-":
+      return subtract(number1, number2);
+    case "*":
+      return multiply(number1, number2);
+    case "/":
+      return divide(number1, number2);
+    case "=":
+      return displayValue;
+  }
+}
+
+function handleOperatorClicked(op) {
+  if (isOperatorClicked) {
+    displayValue = operate();
+    number1 = Number(displayValue);
+    isOperatorClicked = false;
+    display.textContent = displayValue;
+  }
+
+  if(op.id == "=")
+    return;
+
+  if (number1 == undefined)
+    number1 = Number(displayValue);  
+
+  isOperatorClicked = true;
+  operator = op.id;
+
+  number2 = 0;
+  displayValue = "";
+}
+
+function handleKeyClicked(key) {
+  if (isOperatorClicked) {
+    number2 += key.id;
+  }
+  populateDisplay(key);
 }
 
 function addListener() {
-    const keys = document.querySelectorAll('.key');
-    keys.forEach(key =>
-        key.addEventListener("click", function () {
-            populateDisplay(key);
-        })
-    );
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) =>
+    key.addEventListener("click", function () {
+      handleKeyClicked(key);
+    })
+  );
+  const operators = document.querySelectorAll(".operator");
+  operators.forEach((op) =>
+    op.addEventListener("click", function () {
+      handleOperatorClicked(op);
+    })
+  );
 }
 
 function populateDisplay(key) {
-    displayValue += key.id;
-    display.textContent = displayValue;
+  displayValue += key.id;
+
+  display.textContent = displayValue;
 }
 
 addListener();
