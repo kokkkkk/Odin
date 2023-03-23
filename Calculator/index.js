97e742edbb0eb91c9da2fd7678c3dea9ee1acccb
kokkkkk <ko.kiki12355@gmail.com>
2023-03-22 20:01:22 -0400

    Populate when key clikcked

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,32 +1,50 @@
 let number1, number2, operator;
+let displayValue = "";
+const display = document.querySelector('#displayValue');
 
-function add(num1,num2){
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1,num2){
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1,num2){
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1,num2){
+function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(){
-    switch(operator){
-        case '+': 
-            add(number1,number2);
+function operate() {
+    switch (operator) {
+        case "+":
+            add(number1, number2);
             break;
-        case '-':
-            subtract(number1,number2);
+        case "-":
+            subtract(number1, number2);
             break;
-        case '*':
-            multiply(number1,number2);
-        case '/':
-            divide(number1,number2);
+        case "*":
+            multiply(number1, number2);
+        case "/":
+            divide(number1, number2);
     }
 }
+
+function addListener() {
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key =>
+        key.addEventListener("click", function () {
+            populateDisplay(key);
+        })
+    );
+}
+
+function populateDisplay(key) {
+    displayValue += key.id;
+    display.textContent = displayValue;
+}
+
+addListener();
